a82e19a21d07db6d0c624709a7da2b5e47a63d78
Ronildo Sousa <sousaronildo400@gmail.com>
2023-01-17 12:45:01 -0300

    migration, model, factory for products

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'code' => fake()->numerify("#####"),
+            'status' => ['draft', 'trash', 'published'][rand(0, 2)],
+            'imported_t' => now(),
+            'url' => fake()->url(),
+            'creator' => fake()->name(),
+            'created_t' => fake()->numberBetween(),
+            'last_modified_t' => fake()->numberBetween(),
+            'product_name' => fake()->name(),
+            'quantity' => (string)fake()->numberBetween(),
+            'brands' => fake()->name(),
+            'categories' => fake()->name(),
+            'labels' => fake()->sentence(3),
+            'cities' => fake()->city(),
+            'purchase_places' => fake()->city(),
+            'stores' => fake()->name(),
+            'ingredients_text' => fake()->sentence(),
+            'traces' => fake()->sentence(3),
+            'serving_size' => fake()->sentence(3),
+            'serving_quantity' => fake()->numerify(),
+            'nutriscore_score' => fake()->numerify(),
+            'nutriscore_grade' => fake()->word(),
+            'main_category' => fake()->name(),
+            'image_url' => fake()->url(),
+        ];
+    }
+}
