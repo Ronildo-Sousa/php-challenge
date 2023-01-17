c27dad0677ce552eb6e7b3deef26d1f5d62b5642
Ronildo Sousa <sousaronildo400@gmail.com>
2023-01-17 16:59:31 -0300

    adding swagger to project

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
-
+    /**
+     * @OA\Get(
+     *   tags={"Products"},
+     *   path="/api/products",
+     *   summary="Get a list of products",
+     *   description="Returns a paginated list of all products",
+     *   @OA\Response(response=200, description="OK"),
+     * )
+     */
     public function index()
     {
         $products = Product::query()
@@ -22,6 +30,31 @@ class ProductController extends Controller
         //
     }
 
+    /**
+     * @OA\Get(
+     *   tags={"Products"},
+     *   path="/api/products/{code}",
+     *   summary="Get product information",
+     *   description="Returns details of a product based on a code",
+     *   @OA\Parameter(
+     *     name="code",
+     *     description="product code",
+     *     required=true,
+     *     in="path",
+     *     @OA\Schema(
+     *         type="integer"
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *   ),
+     *   @OA\Response(
+     *     response=404, 
+     *     description="Not Found"
+     *   )
+     * )
+     */
     public function show(Product $product)
     {
         return response()->json(['product' => $product], Response::HTTP_OK);
