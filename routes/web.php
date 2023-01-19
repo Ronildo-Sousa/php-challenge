803e5d7f684bff6d9452bccd8c9888564e50eb73
Ronildo <60019987+Ronildo-Sousa@users.noreply.github.com>
2023-01-19 12:51:37 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Actions\Products\ImportFromOpenFoodFacts;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    (new ImportFromOpenFoodFacts)->run();
-    // ImportFromOpenFoodFactsJob::dispatch();
     return view('welcome');
 });
