0098a2bf941f570b9cbf082fa90e89facc588d5f
Ronildo Sousa <sousaronildo400@gmail.com>
2023-01-19 21:30:58 -0300

    refactoring product importation

--- app/Jobs/ImportProductsJob.php
+++ app/Jobs/ImportProductsJob.php
@@ -8,89 +8,49 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
 
 class ImportProductsJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public string $filename;
-
+    private Collection $products;
 
     public function __construct(
         public string $zipFilePath,
         public int $productsAmount = 100
     ) {
-        $this->filename = base_path('/Cron/Products/importedProducts.json');
+        $this->products = collect([]);
     }
 
     public function handle()
     {
-        $products = collect();
         $productsFile = gzfile($this->zipFilePath);
 
         $numProducts = 0;
         $i = 0;
+
         while ($numProducts < $this->productsAmount) {
+            if ($i >= count($productsFile)) {
+                break;
+            }
+
             $product = json_decode($productsFile[$i]);
             $product->code = str_replace('"', '', $product->code);
             $isImported = $this->isImported($product->code);
 
             if (!$isImported) {
-                $products->add([
-                    'code' => $product->code,
-                    'url' => $product->url,
-                    'creator' => $product->creator,
-                    'created_t' => $product->created_t,
-                    'last_modified_t' => $product->last_modified_t,
-                    'product_name' => $product->product_name,
-                    'quantity' => $product->quantity,
-                    'brands' => $product->brands,
-                    'categories' => $product->categories,
-                    'labels' => $product->labels,
-                    'cities' => $product->cities,
-                    'purchase_places' => $product->purchase_places,
-                    'stores' => $product->stores,
-                    'ingredients_text' => $product->ingredients_text,
-                    'traces' => $product->traces,
-                    'serving_size' => $product->serving_size,
-                    'serving_quantity' => $product->serving_quantity,
-                    'nutriscore_score' => $product->nutriscore_score,
-                    'nutriscore_grade' => $product->nutriscore_grade,
-                    'main_category' => $product->main_category,
-                    'image_url' => $product->image_url,
-                ]);
-                $this->saveProductCode(['code' => $product->code]);
+                ImportSingleProductJob::dispatch(collect($product)->toArray());
+
+                $this->products->add($product->code);
                 $numProducts++;
             }
             $i++;
         }
-
-        $products->each(fn ($product) => ImportSingleProductJob::dispatch($product));
     }
 
     public function isImported(string $code)
     {
-        if (!file_exists($this->filename)) {
-            file_put_contents($this->filename, '[]');
-            return false;
-        }
-        $rawCodes = file_get_contents($this->filename);
-        $codes = json_decode($rawCodes);
-
-        return collect($codes)->contains('code', $code);
-    }
-
-    public function saveProductCode(array $data)
-    {
-        if (!file_exists($this->filename)) {
-            file_put_contents($this->filename, '[' . json_encode($data) . ']');
-            return;
-        }
-
-        $currentFile = file_get_contents($this->filename);
-        $content = json_decode($currentFile);
-        $content[] = $data;
-
-        file_put_contents($this->filename, json_encode($content));
+        return $this->products->contains($code);
     }
 }
