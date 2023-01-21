b34621e1410876b55107b3464f1c592721351074
Ronildo Sousa <sousaronildo400@gmail.com>
2023-01-21 11:18:54 -0300

    adds users to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::factory(5)->create();
         Product::factory(10)->create();
     }
 }
